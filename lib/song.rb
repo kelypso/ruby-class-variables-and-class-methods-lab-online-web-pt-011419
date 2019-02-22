541c9ec7829192f38469e4c6e61e6fab16df3556
Kelsey Low <kklowlow@gmail.com>
2019-02-22 05:39:20 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -39,15 +39,15 @@ class Song
   end
 
   def self.artist_count
-    artist_count = {}
+    count = {}
     @@artists.each do |artist|
-      if artist_count[artist] #if the artist is already in the hash
-        artist_count[artist] += 1
+      if count[artist] #if the artist is already in the hash
+        count[artist] += 1
       else
-        artist_count[artist] = 1
+        count[artist] = 1
       end
     end
-    artist_count
+    count
   end
 end
 
