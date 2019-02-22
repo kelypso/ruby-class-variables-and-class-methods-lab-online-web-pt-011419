425f831bd1d9365b7eac9b8e3313eeaeab79b92d
Kelsey Low <kklowlow@gmail.com>
2019-02-22 05:34:27 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -30,7 +30,7 @@ class Song
     # return a hash of genre => count
     count = {}
     @@genres.each do |genre|
-      count[genre] += 1 if count[genre] == true# checking if the genre already exists in the hash
+      count[genre] += 1 if count[genre] == true # checking if the genre already exists in the hash
   end
   
   def self.artist_count
